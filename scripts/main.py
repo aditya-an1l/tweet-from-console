446bbad79150a7a54ae55ad46e1215613a5d7838
aditya-an1l <aditya.anil.productions@gmail.com>
2024-08-22 20:26:29 +0530

    Fix #2 - add tweet.py & modify
    - User can now execute the script by running the tweet.py file
    - better UX, as it avoids unnecessary "cd"s

--- scripts/main.py
+++ scripts/main.py
@@ -36,6 +36,7 @@ logging.basicConfig(
 
 def log_error(error_message):
     """logs the error message in a file"""
+
     console.log(error_message)
     console.print(
         " \n \n [magenta]=== Complete Error Log === [/magenta] ", justify="center"
@@ -49,6 +50,7 @@ def log_error(error_message):
 
 def tweet_confirmation_alert(tweet, tweet_log=False):
     """Returns the just sent tweet in console"""
+
     confirmation_text = f'The tweet [cyan]{tweet}[/cyan] posted at {datetime.now().strftime("%H:%M:%S %Y-%m-%d")}'
     console.print(confirmation_text)
     if tweet_log:
@@ -60,6 +62,7 @@ def tweet_confirmation_alert(tweet, tweet_log=False):
 
 def check_api_keys(keys):
     """check if API keys are missing"""
+
     missing_keys = [key for key, value in keys.items() if not value]
     if missing_keys:
         error_message = f" [red] Missing Keys: {', '.join(missing_keys)} [/red]. Try checking the .env file in {log_directory}"
@@ -69,6 +72,7 @@ def check_api_keys(keys):
 
 def send_tweet(tweet):
     """Send the Tweet"""
+
     tweet = tweet
     console.print(f'You sure you want to post "[cyan]{tweet}[/cyan]" ?  ')
     console.print("[green] [ (Y)es / (N)o ] : [/green] ")
@@ -85,49 +89,37 @@ def send_tweet(tweet):
         send_tweet(tweet)
 
 
-try:
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+def authentication():
+    """Assign and authenticate key"""
+    try:
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        access_token = os.getenv("ACCESS_TOKEN")
+        access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    check_api_keys(
-        {
-            "CONSUMER_KEY": consumer_key,
-            "CONSUMER_SECRET": consumer_secret,
-            "ACCESS_TOKEN": access_token,
-            "ACCESS_TOKEN_SECRET": access_token_secret,
-        }
+        check_api_keys(
+            {
+                "CONSUMER_KEY": consumer_key,
+                "CONSUMER_SECRET": consumer_secret,
+                "ACCESS_TOKEN": access_token,
+                "ACCESS_TOKEN_SECRET": access_token_secret,
+            }
+        )
+    except ValueError:
+        sys.exit(1)
+
+    # Authentication
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    client = tweepy.Client(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
     )
-except ValueError:
-    sys.exit(1)
 
 
-# Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-client = tweepy.Client(
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token=access_token,
-    access_token_secret=access_token_secret,
-)
-
-
-try:
-    tweet = sys.argv[1]
-except IndexError:
-    log_error(
-        "Error: No tweet provided. Please provide a tweet as a command-line argument."
-    )
-    sys.exit(1)
-except TypeError:
-    log_error("Error: The tweet must be a string.")
-    sys.exit(1)
-except ValueError as ve:
-    log_error(f"Error: {ve}")
-    sys.exit(1)
-
-send_tweet(tweet)
+if __name__ == "__main__":
+    send_tweet(tweet)
